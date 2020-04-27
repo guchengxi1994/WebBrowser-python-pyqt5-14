9b08469b7f525164878e2094f57f84554bff8701
guchengxi1994 <528490652@qq.com>
2020-04-27 15:30:04 +0800

    update

---
+++ test.py
@@ -0,0 +1,10 @@
+'''
+@lanhuage: python
+@Descripttion: 
+@version: beta
+@Author: xiaoshuyui
+@Date: 2020-04-27 15:29:02
+@LastEditors: xiaoshuyui
+@LastEditTime: 2020-04-27 15:29:40
+'''
+print("A is larger than C")
